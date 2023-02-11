d936ab1b2e071a53642a3dac528cee1a9ec215c7
monoha724 <bunnyalex724@gmail.com>
2023-02-11 19:35:59 +0900

    main page first commit

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-client = MongoClient("mongodb+srv://hanghaeteam14:team14@cluster0.7iskixj.mongodb.net/?retryWrites=true&w=majority",
+client = MongoClient("mongodb+srv://test:sparta@cluster0.do6jypk.mongodb.net/?retryWrites=true&w=majority",
                      tlsCAFile=ca)
 
 
+if __name__ == '__main__':
+app.run('0.0.0.0', port=5000, debug=True)
